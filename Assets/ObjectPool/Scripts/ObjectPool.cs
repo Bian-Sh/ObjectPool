e380a0ec374be08eeed94f92621626b87e0e210d
Bian-Sh <bshsf@qq.com>
2020-11-11 12:00:28 +0800

    Update ObjectPool.cs
    1. 简化代码，parent 可以不用判 null直接传值到setparent方法。
    2. 修复bug：Recycle 方法中调用SetParent需要区别对待回收对象，UI需要第二个参数为false

--- Assets/ObjectPool/Scripts/ObjectPool.cs
+++ Assets/ObjectPool/Scripts/ObjectPool.cs
@@ -30,10 +30,8 @@ public sealed class ObjectPool : MonoBehaviour
     }
 
     static List<GameObject> tempList = new List<GameObject>();
-
     Dictionary<GameObject, List<GameObject>> pooledObjects = new Dictionary<GameObject, List<GameObject>>();
     Dictionary<GameObject, GameObject> spawnedObjects = new Dictionary<GameObject, GameObject>();
-
     public StartupPoolMode startupPoolMode;
     public StartupPool[] startupPools;
 
@@ -42,7 +40,6 @@ public sealed class ObjectPool : MonoBehaviour
     {
         if (_instance != null && _instance.gameObject != gameObject)
         {
-            Debug.Log("创造了新的克隆体！");
             if (Application.isPlaying)
             {
                 Destroy(gameObject);
@@ -132,11 +129,7 @@ public sealed class ObjectPool : MonoBehaviour
                 if (obj != null)
                 {
                     trans = obj.transform;
-                    if (parent != null)
-                    {
-                        trans.SetParent(parent, !(parent is RectTransform));
-                        trans.localScale = prefab.transform.localScale;
-                    }
+                    trans.SetParent(parent, !(parent is RectTransform));
                     trans.localPosition = position;
                     trans.localRotation = rotation;
                     obj.SetActive(true);
@@ -146,11 +139,7 @@ public sealed class ObjectPool : MonoBehaviour
             }
             obj = Instantiate(prefab);
             trans = obj.transform;
-            if (parent != null)
-            {
-                trans.SetParent(parent, !(parent is RectTransform));
-                trans.localScale = prefab.transform.localScale;
-            }
+            trans.SetParent(parent, !(parent is RectTransform));
             trans.localPosition = position;
             trans.localRotation = rotation;
             obj.SetActive(true);
@@ -161,11 +150,7 @@ public sealed class ObjectPool : MonoBehaviour
         {
             obj = Instantiate(prefab);
             trans = obj.GetComponent<Transform>();
-            if (parent != null)
-            {
-                trans.SetParent(parent, !(parent is RectTransform));
-                trans.localScale = prefab.transform.localScale;
-            }
+            trans.SetParent(parent, !(parent is RectTransform));
             trans.localPosition = position;
             trans.localRotation = rotation;
             return obj;
@@ -184,7 +169,7 @@ public sealed class ObjectPool : MonoBehaviour
     {
         Instance.pooledObjects[prefab].Add(obj);
         Instance.spawnedObjects.Remove(obj);
-        obj.transform.SetParent(Instance.transform);
+        obj.transform.SetParent(Instance.transform,!(obj.transform is RectTransform));
         obj.SetActive(false);
     }
 
@@ -302,7 +287,6 @@ public static class ObjectPoolExtensions
     public static void CreatePool<T>(this T prefab, int initialPoolSize) where T : Component => ObjectPool.CreatePool(prefab, initialPoolSize);
     public static void CreatePool(this GameObject prefab) => ObjectPool.CreatePool(prefab, 0);
     public static void CreatePool(this GameObject prefab, int initialPoolSize) => ObjectPool.CreatePool(prefab, initialPoolSize);
-
     public static T Spawn<T>(this T prefab, Transform parent, Vector3 position, Quaternion rotation) where T : Component => ObjectPool.Spawn(prefab, parent, position, rotation);
     public static T Spawn<T>(this T prefab, Vector3 position, Quaternion rotation) where T : Component => ObjectPool.Spawn(prefab, null, position, rotation);
     public static T Spawn<T>(this T prefab, Transform parent, Vector3 position) where T : Component => ObjectPool.Spawn(prefab, parent, position, Quaternion.identity);
@@ -315,36 +299,28 @@ public static class ObjectPoolExtensions
     public static GameObject Spawn(this GameObject prefab, Vector3 position) => ObjectPool.Spawn(prefab, null, position, Quaternion.identity);
     public static GameObject Spawn(this GameObject prefab, Transform parent) => ObjectPool.Spawn(prefab, parent, Vector3.zero, Quaternion.identity);
     public static GameObject Spawn(this GameObject prefab) => ObjectPool.Spawn(prefab, null, Vector3.zero, Quaternion.identity);
-
     public static void Recycle<T>(this T obj) where T : Component => ObjectPool.Recycle(obj);
     public static void Recycle(this GameObject obj) => ObjectPool.Recycle(obj);
-
     public static void RecycleAll<T>(this T prefab) where T : Component => ObjectPool.RecycleAll(prefab);
     public static void RecycleAll(this GameObject prefab) => ObjectPool.RecycleAll(prefab);
-
     public static int CountPooled<T>(this T prefab) where T : Component => ObjectPool.CountPooled(prefab);
     public static int CountPooled(this GameObject prefab) => ObjectPool.CountPooled(prefab);
-
     public static int CountSpawned<T>(this T prefab) where T : Component => ObjectPool.CountSpawned(prefab);
     public static int CountSpawned(this GameObject prefab) => ObjectPool.CountSpawned(prefab);
-
     public static List<GameObject> GetSpawned(this GameObject prefab, List<GameObject> list, bool appendList) => ObjectPool.GetSpawned(prefab, list, appendList);
     public static List<GameObject> GetSpawned(this GameObject prefab, List<GameObject> list) => ObjectPool.GetSpawned(prefab, list, false);
     public static List<GameObject> GetSpawned(this GameObject prefab) => ObjectPool.GetSpawned(prefab, null, false);
     public static List<T> GetSpawned<T>(this T prefab, List<T> list, bool appendList) where T : Component => ObjectPool.GetSpawned(prefab, list, appendList);
     public static List<T> GetSpawned<T>(this T prefab, List<T> list) where T : Component => ObjectPool.GetSpawned(prefab, list, false);
     public static List<T> GetSpawned<T>(this T prefab) where T : Component => ObjectPool.GetSpawned(prefab, null, false);
-
     public static List<GameObject> GetPooled(this GameObject prefab, List<GameObject> list, bool appendList) => ObjectPool.GetPooled(prefab, list, appendList);
     public static List<GameObject> GetPooled(this GameObject prefab, List<GameObject> list) => ObjectPool.GetPooled(prefab, list, false);
     public static List<GameObject> GetPooled(this GameObject prefab) => ObjectPool.GetPooled(prefab, null, false);
     public static List<T> GetPooled<T>(this T prefab, List<T> list, bool appendList) where T : Component => ObjectPool.GetPooled(prefab, list, appendList);
     public static List<T> GetPooled<T>(this T prefab, List<T> list) where T : Component => ObjectPool.GetPooled(prefab, list, false);
     public static List<T> GetPooled<T>(this T prefab) where T : Component => ObjectPool.GetPooled(prefab, null, false);
-
     public static void DestroyPooled(this GameObject prefab) => ObjectPool.DestroyPooled(prefab);
     public static void DestroyPooled<T>(this T prefab) where T : Component => ObjectPool.DestroyPooled(prefab.gameObject);
-
     public static void DestroyAll(this GameObject prefab) => ObjectPool.DestroyAll(prefab);
     public static void DestroyAll<T>(this T prefab) where T : Component => ObjectPool.DestroyAll(prefab.gameObject);
 }
